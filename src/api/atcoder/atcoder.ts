c4d88b1025a4951aed098c6baefbaa31f4de7507
abdealijaroli <jaroliabdeali@gmail.com>
2021-04-10 14:46:18 +0530

    Atcoder integrated

---
+++ src/api/atcoder/atcoder.ts
@@ -0,0 +1,30 @@
+import axios from 'axios'
+import { UpcomingContestResponse, ContestResponseSchema } from './interfaces'
+import { constants } from '../../constants'
+import { redisGet, redisSet } from '../../app'
+
+export async function upcomingContestsAtcoder (): Promise<UpcomingContestResponse> {
+  const re = await redisGet('ac_upcoming')
+  if (re) {
+    return {
+      result: JSON.parse(re)
+    }
+  } else {
+    const response = await axios.get(constants.atCoderUrl)
+    const res = response.data
+    const at: Array<ContestResponseSchema> = []
+    try {
+      for (const item of res.events) {
+        at.push(item)
+      }
+    } catch (err) {
+      console.log(constants.atCoderErr)
+    }
+    if (at.length > 0) {
+      await redisSet('ac_upcoming', JSON.stringify(at))
+    }
+    return {
+      result: at
+    }
+  }
+}
